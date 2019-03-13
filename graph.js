9dd9d5304f8d9d2b9c1e7855f0382dcb1e3ef317
Kleckov Gleb <d91580433832@yandex.ru>
2019-03-13 11:04:04 +0300

    Update 12 of March

---
+++ graph.js
@@ -0,0 +1,110 @@
+class Graph{
+    constructor(obj){
+        this.obj = obj
+    }
+
+    showData(){
+        return this.obj
+    }
+
+    // Find maximum value of the charts in graph
+    defineMax(){
+        const  arrMax = []
+
+        for(let i = 1; i < this.obj.columns.length; i++){
+            let temp = this.obj.columns[i].splice(1, this.obj.columns[i].length)
+
+            arrMax.push(Math.max(...temp))
+            this.obj.columns[i].push(...temp)
+        }
+        let max = Math.max(...arrMax) // Max value of arrays
+
+        return max
+    }
+
+    colorElement(element, firstElementOfArray){
+        const color = this.obj.colors[firstElementOfArray]
+        if(element.tagName === 'circle'){
+            element.setAttributeNS(null, 'fill', `${color}`)
+        }
+
+        else{
+            element.setAttributeNS(null, 'stroke', `${color}`)
+        }
+    }
+
+    // Build Chrats
+    // TODO: REfactor % add <g> and append paths to this tag
+    // TODO: also set the stroke and so on to this tag
+    addToPath(selector){
+        const SVG = document.querySelector(`.${selector}`)
+        const rectSVG = SVG.getBoundingClientRect()
+        const intervalBetweenPoints = rectSVG.width / this.obj.columns[0].length
+
+        const max = this.defineMax()
+
+        this.obj.columns.forEach((array, index) => {
+            if(index > 0){
+                let xPos = 0
+
+                for(let i = 2; i < array.length;i++){
+                    const curr = array[i]
+                    const prev = array[i - 1]
+
+                    let posOnTheScreen
+                    let prevPercent = parseInt(prev * 100 / max)
+                    let prevPos = parseInt(rectSVG.height * prevPercent / 100)
+
+
+                    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+                    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+
+
+
+                    if(curr === max){
+                        posOnTheScreen = 2
+                        path.setAttributeNS(null, 'd', `M${xPos - intervalBetweenPoints}, ${prevPos} L${xPos}, ${posOnTheScreen}`)
+
+                        this.colorElement(path, this.obj.columns[index][0])
+
+                        circle.setAttributeNS(null, 'cx', `${xPos - intervalBetweenPoints}`)
+                        circle.setAttributeNS(null, 'cy', `${prevPos}`)
+                        circle.setAttributeNS(null, 'r', "1")
+                        this.colorElement(circle, this.obj.columns[index][0])
+
+                        SVG.appendChild(path)
+                        SVG.appendChild(circle)
+                    }
+
+                    else{
+
+                        let percent = parseInt((curr * 100) / max);
+                        posOnTheScreen = parseInt((rectSVG.height * percent) / 100);
+
+                        if(prev === max) prevPos = 2
+
+                        // Add path
+                        const path = document.createElementNS('http://www.w3.org/2000/svg','path')
+                        path.setAttributeNS(null,'d', `M${xPos - intervalBetweenPoints}, ${prevPos} L${xPos}, ${posOnTheScreen}`)
+
+                        this.colorElement(path, this.obj.columns[index][0])
+
+                        circle.setAttributeNS(null, 'cx', `${xPos - intervalBetweenPoints}`)
+                        circle.setAttributeNS(null, 'cy', `${prevPos}`)
+                        circle.setAttributeNS(null, 'r', "2")
+
+                        this.colorElement(circle, this.obj.columns[index][0])
+
+
+                        SVG.appendChild(path)
+                        SVG.appendChild(circle)
+                    }
+
+                    xPos += intervalBetweenPoints
+                }
+            }
+        })
+
+
+    }
+}
--- index.js
+++ index.js
@@ -1,114 +1,76 @@
-let DATA
-async function getData() {
-    // fetch('chart_data.json')
-    //     .then(res => res.json())
-    //     .then(res => DATA = res)
-    //     .catch(err => console.log(err))
-}
+let DATA // DATA form JSON file
 
+function setTheCoreners(){
+    const corners = document.querySelectorAll('.selectCorner')
+    for(let i = 0; i < corners.length; i++){
+        corners[i].addEventListener('', ()=>{
 
-//TODO: delete it! 
-function randomInRange(start, end) {
-  return Math.floor(Math.random() * (end - start + 1) + start);
-}
-
-const arr = []
-
-for(let i = 0; i < 20; i++){
- arr.push(randomInRange(40,200))
-}
-
-const numberOfPoints = arr.length
-
-const intervalBetweenPoints = parseInt(innerWidth / numberOfPoints)
-
-const arrMax = Math.max(...arr)
-
-function addToPath(curr, prev, xPos, selector) {
-
-    let posOnTheScreen
-    const SVG = document.querySelector(`${selector}`)
-    let rectSVG = SVG.getBoundingClientRect()
-
-    let prevPercent = parseInt(prev * 100 / arrMax)
-    let prevPos = parseInt(rectSVG.height * prevPercent / 100)
-
-
-    // IF the element max on the screen
-    if (curr === arrMax) {
-        posOnTheScreen = 0
-
-        // Add path
-        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-        
-        path.setAttributeNS(null, 'd', `M${xPos - intervalBetweenPoints}, ${prevPos} L${xPos}, ${posOnTheScreen}`)
-        path.setAttributeNS(null,'filter', 'url(#blueMe)')
-        
-        
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-        circle.setAttributeNS(null, 'cx', `${xPos - intervalBetweenPoints}`)
-        circle.setAttributeNS(null, 'cy', `${prevPos}`)
-        circle.setAttributeNS(null, 'r', "2")
-
-        
-        SVG.appendChild(path)
-        SVG.appendChild(circle)
+        })
     }
-
-    else {
-
-        // Position of the current path
-        let percent = parseInt((curr * 100) / arrMax); 
-        posOnTheScreen = parseInt((rectSVG.height * percent) / 100);
-
-        if(prev === arrMax) prevPos = 0
-
-        // Add path
-        const path = document.createElementNS('http://www.w3.org/2000/svg','path')
-        path.setAttributeNS(null,'d', `M${xPos - intervalBetweenPoints}, ${prevPos} L${xPos}, ${posOnTheScreen}`)
-        
-        const circle = document.createElementNS("http://www.w3.org/2000/svg","circle")
-        circle.setAttributeNS(null,'cx', `${xPos - intervalBetweenPoints}`)
-        circle.setAttributeNS(null,'cy', `${prevPos}`)
-        circle.setAttributeNS(null,'r', "2")
-
-        SVG.appendChild(path)
-        SVG.appendChild(circle)
-    }
-}
-
-function buildChart(){
-
 }
 
 window.onload = async () => {
-    // DATA.forEach(object => {
-    //     object.columns.forEach(xpos => console.log(xpos))
-    // })
 
     await fetch('chart_data.json')
         .then(res => res.json())
         .then(res => DATA = res)
         .catch(err => console.log(err))
 
-    DATA.forEach(object => {
-        object.columns.forEach(xpos => console.log(xpos))
-    })
+    // TODO: Change DATA[0] to index thaty client is choosed
+    let obj = DATA[0]
+    const firstGraph = new Graph(obj)
+    firstGraph.addToPath('smallSVG')
+    firstGraph.addToPath('bigSVG')
 
+    const selectorCorners = document.querySelectorAll('.selectCorner')
+    for(let i = 0; i < selectorCorners.length; i++){
+        // makeDraggable(selectorCorners[i])
 
-    // const SVG = document.querySelector('svg')
-    // SVG.style.width = innerWidth
-    // let selector = '.bigChart'
-    //
-    //
-    // // Change it for number of elements on the screen
-    // //TODO: refactor ; make function to build charts in for loop
-    // let xPos = 0
-    // for (let i = 1; i < arr.length; i++) {
-    //     addToPath(arr[i], arr[i - 1], xPos, selector)
-    //     addToPath(arr[i], arr[i-1],xPos,'.smallChart')
-    //     xPos += intervalBetweenPoints
-    // }
+        // moveSection(selectorCorners[i],'350','0')
+    }
 }
+
+
+// TODO: stop here trying to make selector corners move right
+function makeDraggable(elem){
+    let svg = elem;
+    svg.addEventListener('mousedown', startDrag);
+    svg.addEventListener('mousemove', drag);
+    svg.addEventListener('mouseup', endDrag);
+    svg.addEventListener('mouseleave', endDrag);
+
+    let selectedElement = false
+    let offset
+
+    let prevX
+
+    function startDrag(elem) {
+        if(elem.classList.contains('selectCorner')){
+            selectedElement = elem
+        }
+    }
+
+    function getMousePosition(elem) {
+
+    }
+
+    function drag(elem) {
+        let transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
+        elem.setAttribute('transform', transformAttr);
+    }
+
+    function endDrag(elem) {
+
+    }
+}
+
+// function moveSection(elem, xOffset, yOffset) {
+//
+//     if (elem.classList.contains('selectCorner')) {
+//         let transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
+//         elem.setAttribute('transform', transformAttr);
+//     }
+// }
+
 
 
